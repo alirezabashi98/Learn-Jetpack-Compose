a359ebfa4c8dcb6d6f5a40754f4ef903b8f1b8d2
alirezabashi98 <alirezabashi98@gmail.com>
2021-11-04 17:23:18 +0330

    Remove extra code

--- app/src/main/java/com/alireza/bashi/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/alireza/bashi/jetpackcompose/MainActivity.kt
@@ -14,25 +14,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            DefaultPreview()
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposeTheme {
-        Greeting("Android")
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+            Text(text = "Hello Android!")
+        }
     }
 }
